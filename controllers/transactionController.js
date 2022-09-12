46581b02c21e1390e6aa0efd16c4112fe629c58b
LubegaH <lubegah@gmail.com>
2022-09-12 22:36:20 +0300

    Add controller for adding and deleting trans

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,3 +4,17 @@
 exports.getTransactions = (req, res, next) => {
   res.send('GET transactions');
 };
+
+// @desc    Add transaction
+// @route   POST /api/v1/transactions
+// @access  Public
+exports.addTransactions = (req, res, next) => {
+  res.send('POST transactions');
+};
+
+// @desc    Delete transaction
+// @route   DELETE /api/v1/transaction/:id
+// @access  Public
+exports.getTransactions = (req, res, next) => {
+  res.send('DELETE transaction');
+};
